bc780e9766b7b959370d989cb1b482b30b444389
Calrider <9564228+Calrider@users.noreply.github.com>
2020-05-13 20:09:12 -0700

    Updated setup.py, version=0.0.8

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="webull",
-    version="0.0.7",
+    version="0.0.8",
     author="ted chou",
     description="The unofficial python interface for the WeBull API",
     license='MIT',
