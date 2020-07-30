df91ed12bdc516461d456a748aadc5c1c4762213
vpatil <vindows18@gmail.com>
2020-07-30 21:01:32 +0530

    add: get watchlists

--- webull/endpoints.py
+++ webull/endpoints.py
@@ -141,3 +141,6 @@ class urls :
 
     def social_home(self, topic, num=100):
         return f'{self.base_user_url}/social/feed/topic/{topic}/home?size={num}'
+
+    def portfolio_lists(self):
+        return f'{self.base_options_gw_url}/personal/portfolio/v2/check'
