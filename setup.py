e6c9640c4cc9377edb924e5a7a507e66b96b90ca
Ted Chou <>
2021-11-05 20:38:43 +0900

    test_deploy

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="webull",
-    version="0.3.8",
+    version="0.3.9",
     author="ted chou",
     description="The unofficial python interface for the WeBull API",
     license='MIT',
