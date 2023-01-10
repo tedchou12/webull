d3a8ee55cde2ee32716578401d082ea60cc1998d
Ted <ted.chou12@gmail.com>
2023-01-11 01:22:17 +0900

    Update setup.py
    bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="webull",
-    version="0.6.0",
+    version="0.6.1",
     author="ted chou",
     description="The unofficial python interface for the WeBull API",
     license='MIT',
